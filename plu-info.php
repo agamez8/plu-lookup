90a16b7e5db0954295b28fe23cbf2ee3d04cef63
alan <agamez8@miners.utep.edu>
2022-02-20 22:58:40 -0700

    Added manager login, refactoring php file

--- plu-info.php
+++ plu-info.php
@@ -1,23 +1,42 @@
-<html lang="en">
-<body>
-
 <?php
 
 /*
- * Function to get customer input
+ * Function to display items in inventory
  */
-function getInfo()
+function displayItems()
+{
+
+}
+
+/*
+ * Store manager login info
+ */
+function managerLogin()
+{
+    $userIn = $_POST['code'];
+
+    if (isset($_POST['login']) && !empty($_POST['code'])) {
+        if ($_POST['code'] == '1234') {
+            header('Refresh: 2; URL = plu-menu.html');
+        }else {
+            header('Refresh: 2; URL = manager-page.html');
+            exit;
+        }
+    }
+//    echo $userIn . "</br>";
+}
+
+/*
+ * Function to add new items to the list
+ */
+function addItems()
 {
     $itemName = $_POST['name']; // Item name input saved
     $pluNum = $_POST['number']; // PLU code input saved
 
-    echo $itemName . "</br>";
-    echo $pluNum . "</br>";
 }
 
-getInfo();
+displayItems();
+managerLogin();
 
 ?>
-
-</body>
-</html>
